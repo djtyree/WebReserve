8aa8842de7c55e7abed51cb201a92ee0912cb384
djtyree <djtyree@alumni.clemson.edu>
2013-04-05 13:40:29 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WebReserve::Application.config.secret_token = 'f647678772859e2cba8093986dd751c25ab49317d123c22633c4a3707b281f6260abfe9465169b42a785555ed3d1d130fba51c147005dabc9ae07b048014542e'
